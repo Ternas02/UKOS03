b455f9ba5fec23481f146150c36cde1b55bb5cfd
Ternas02 <93030257+Ternas02@users.noreply.github.com>
2021-11-06 14:53:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
+
 #include <iostream>
 
-auto main () -> int
+auto main () -> int {
 
 int x, y, z;
 std::cout << "Podaj 3 liczby calkowite" << std::endl;
+std::cout <<  "Podaj pierwsza liczbe" << std::endl;
 std::cin >> x;
+std::cout << "Podaj druga liczbe" << std::endl;
 std::cin >> y;
+std::cout << "Podaj trzecia liczbe" << std::endl;
 std::cin >> z;
 
 std::cout << "Srednia tych liczb wynosi " <<(x + y + z)/3 << std::endl;
 
 return 0;
+
+}
+
 
